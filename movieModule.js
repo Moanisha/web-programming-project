8ec3eb269a6d5b4fc39f37d2235df45671c82038
Moanisha <moanisha94@gmail.com>
2022-12-01 12:42:42 -0500

    Added update and create

--- movieModule.js
+++ movieModule.js
@@ -1,3 +1,10 @@
+/**********************************************************************************************
+ * ITE5315 – Project *
+ * I declare that this assignment is my own work in accordance with Humber Academic Policy. *
+ * No part of this assignment has been copied manually or electronically from any other source * 
+ * (including web sites) or distributed to other students. *
+ * Group member Name: Moanisha Velayuthem, Sakshi Patel Student IDs: N01482302, N01551583 Date: 30-11-2022 *
+ * ********************************************************************************************/
 
 const Movie = require('./models/movie');
 
@@ -81,22 +88,24 @@ const getMoviesById = async (req, res) => {
 }
 
 const deleteMovieById = async (req, res) => {
+    let movie;
     let id = req.params.delmovie_id;
-    try{
+    try {
         movie = await Movie.deleteOne({
             _id: id
         })
-        if(movie.deletedCount == 0) {
+        if (movie.deletedCount == 0) {
             return res.status(404).send("Cannot find movie with ID");
         } else {
             return res.send('Movie data has been sucessfully deleted!');
-        }    
-    } catch(err) {
+        }
+    } catch (err) {
         return res.status(500).json({ message: err.message });
     }
 }
 
 const updateMovieById = async (req, res) => {
+    let movie;
     // create mongose method to update an existing record into collection
     let id = req.params.movie_id;
     var data = {
@@ -119,47 +128,45 @@ const updateMovieById = async (req, res) => {
         type: req.body.type,
         tomatoes: req.body.tomatoes
     }
+    try {
+        movie = await Movie.findByIdAndUpdate(id, data);
+        return res.status(200).send("Movie has been updated");
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
 
-    // save the movie
-    Movie.findByIdAndUpdate(id, data, function (err, movies) {
-        if (err) throw err;
-        return res.send('Successfully updated!!- ' + movies);
-    });
 }
 
-const createMovie = async (req,res) => {
-     // create mongoose method to create a new record into collection
-     console.log(req.body);
-     let data = {
-         plot: req.body.plot,
-         genres: req.body.genres,
-         runtime: req.body.runtime,
-         cast: req.body.cast,
-         poster: req.body.poster,
-         num_mflix_comments: req.body.num_mflix_comments,
-         title: req.body.title,
-         countries: req.body.countries,
-         languages: req.body.languages,
-         released: req.body.released,
-         directors: req.body.directors,
-         rated: req.body.rated,
-         awards: req.body.awards,
-         lastupdated: req.body.lastupdated,
-         year: req.body.year,
-         imdb: req.body.imdb,
-         type: req.body.type,
-         tomatoes: req.body.tomatoes
-     }
-     Movie.create(data, function (err, movies) {
-         if (err)
-             return res.send(err);
- 
-         // get and return all the movies after newly created movie record
-         Movie.find(function (err, movies) {
-             if (err)
-                 res.send(err)
-             return res.json(movies);
-         });
-     }); 
+const createMovie = async (req, res) => {
+    let movie;
+    // create mongoose method to create a new record into collection
+    console.log(req.body);
+    let data = {
+        plot: req.body.plot,
+        genres: req.body.genres,
+        runtime: req.body.runtime,
+        cast: req.body.cast,
+        poster: req.body.poster,
+        num_mflix_comments: req.body.num_mflix_comments,
+        title: req.body.title,
+        countries: req.body.countries,
+        languages: req.body.languages,
+        released: req.body.released,
+        directors: req.body.directors,
+        rated: req.body.rated,
+        awards: req.body.awards,
+        lastupdated: req.body.lastupdated,
+        year: req.body.year,
+        imdb: req.body.imdb,
+        type: req.body.type,
+        tomatoes: req.body.tomatoes
+    }
+    try {
+        movie = await Movie.create(data);
+        return res.send("Movie created");
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+
 }
 module.exports = { getAllMovies, getAllMoviesForm, getMoviesById, deleteMovieById, updateMovieById, createMovie };
